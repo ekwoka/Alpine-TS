7d1e6a94f6630f5920f3915a55a3c9f5bbdd80b3
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-08 12:42:54 +0400

    :construction_worker: Updates how package sizes are computed

--- scripts/getSize.js
+++ scripts/getSize.js
@@ -5,35 +5,42 @@ import { join } from 'node:path';
 import prettyBytes from 'pretty-bytes';
 
 const test = process.env.NODE_ENV === 'test';
-build({
-  entryPoints: ['./packages/alpinejs/dist/index.js'],
-  inject: [],
-  outfile: 'testing/bundle.js',
-  write: test,
-  splitting: false,
-  format: 'esm',
-  bundle: true,
-  target: 'esnext',
-  platform: 'browser',
-  minify: true,
-  watch: false,
-  plugins: [],
-  mainFields: ['module', 'main'],
-}).then(async ({ outputFiles }) => {
-  if (test) return;
+
+const packages = ['alpinejs'];
+
+const bundleData = await Promise.all(
+  packages.map(async (pkg) => [pkg, await bundleCode(pkg)])
+);
+if (test) process.exit(0);
+const content = JSON.stringify(Object.fromEntries(bundleData), null, 2);
+await writeFile(join('size.json'), content, 'utf8');
+
+async function bundleCode(pkg) {
+  const { outputFiles } = await build({
+    entryPoints: [`./packages/${pkg}/dist/index.js`],
+    inject: [],
+    outfile: 'testing/bundle.js',
+    write: test,
+    splitting: false,
+    format: 'esm',
+    bundle: true,
+    target: 'esnext',
+    platform: 'browser',
+    minify: true,
+    watch: false,
+    plugins: [],
+    mainFields: ['module', 'main'],
+  });
+  if (test) return 'done';
   const { minified, gzipped } = await getSizes(outputFiles[0].contents);
-  const content = JSON.stringify(
-    {
-      minified,
-      gzipped,
-    },
-    null,
-    2
+  console.log(
+    `${pkg}: Bundle: ${minified.pretty}, Minzipped: ${gzipped.pretty}`
   );
-  await writeFile(join('size.json'), content, 'utf8');
-  console.log(`New Package size: ${minified.pretty}`);
-  console.log(`Minzipped size: ${gzipped.pretty}`);
-});
+  return {
+    minified,
+    gzipped,
+  };
+}
 
 function sizeInfo(bytesSize) {
   return {
