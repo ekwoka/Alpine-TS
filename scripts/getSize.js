561c3f98657f30fe2d871dded6aad7ac8c464433
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-20 16:01:07 +0400

    :ambulance: Adjusts Scripts

--- scripts/getSize.js
+++ scripts/getSize.js
@@ -8,14 +8,7 @@ const test = process.env.NODE_ENV === 'test';
 
 const packages = ['alpinejs'];
 
-const bundleData = await Promise.all(
-  packages.map(async (pkg) => [pkg, await bundleCode(pkg)])
-);
-if (test) process.exit(0);
-const content = JSON.stringify(Object.fromEntries(bundleData), null, 2);
-await writeFile(join('size.json'), content, 'utf8');
-
-async function bundleCode(pkg) {
+const bundleCode = async (pkg) => {
   const { outputFiles } = await build({
     entryPoints: [`./packages/${pkg}/dist/index.js`],
     inject: [],
@@ -40,22 +33,25 @@ async function bundleCode(pkg) {
     minified,
     gzipped,
   };
-}
+};
 
-function sizeInfo(bytesSize) {
-  return {
-    pretty: prettyBytes(bytesSize),
-    raw: bytesSize,
-  };
-}
+const sizeInfo = (bytesSize) => ({
+  pretty: prettyBytes(bytesSize),
+  raw: bytesSize,
+});
 
-function getBytes(str) {
-  return Buffer.byteLength(str, 'utf8');
-}
+const getBytes = (str) => Buffer.byteLength(str, 'utf8');
 
-async function getSizes(code) {
+const getSizes = async (code) => {
   const minifiedSize = getBytes(code);
   const gzippedSize = await gzipSize(code);
 
   return { minified: sizeInfo(minifiedSize), gzipped: sizeInfo(gzippedSize) };
-}
+};
+
+const bundleData = await Promise.all(
+  packages.map(async (pkg) => [pkg, await bundleCode(pkg)])
+);
+if (test) process.exit(0);
+const content = JSON.stringify(Object.fromEntries(bundleData), null, 2);
+await writeFile(join('size.json'), content, 'utf8');
