704ef9897096282f28d90e3865799d9d33ea45e6
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-01-02 11:00:37 +0900

    :white_check_mark: Tests x-bind style

---
+++ tests/directives/x-bind:style.test.ts
@@ -0,0 +1,131 @@
+import { noop, render } from '../utils';
+import { describe, expect, it } from 'vitest';
+
+describe('x-bind:style', () => {
+  it('binds from object', async () => {
+    const { $ } = await render(
+      noop,
+      `<div x-data>
+            <span x-bind:style="{ color: 'red' }">I should be red</span>
+        </div>`
+    );
+    expect($('span').getAttribute('style')).toBe('color: red;');
+  });
+  it('binds objects with camelCase', async () => {
+    const { $ } = await render(
+      noop,
+      `<div x-data>
+        <span x-bind:style="{ backgroundColor: 'red' }">I should be red</span>
+      </div>`
+    );
+    expect($('span').getAttribute('style')).toBe('background-color: red;');
+  });
+  it('binds objects with kebab-case', async () => {
+    const { $ } = await render(
+      noop,
+      `<div x-data>
+        <span x-bind:style="{ 'background-color': 'red' }">I should be red</span>
+      </div>`
+    );
+    expect($('span').getAttribute('style')).toBe('background-color: red;');
+  });
+  it('binds objects with CSS variables', async () => {
+    const { $, window } = await render(
+      noop,
+      `<div x-data x-bind:style="{ '--MyCSS-Variable': 0.25 }">
+        <span style="opacity: var(--MyCSS-Variable);">I should be hardly visible</span>
+      </div>`
+    );
+    expect(window.getComputedStyle($('span')).opacity).toBe('0.25');
+    expect();
+  });
+  it('merges bindings with existing styles', async () => {
+    const { $ } = await render(
+      noop,
+      `<div x-data>
+        <span style="display: block;" x-bind:style="{ color: 'red' }">I should be red</span>
+      </div>`
+    );
+    expect($('span').getAttribute('style')).toBe('display: block; color: red;');
+  });
+});
+
+/* import { beHidden, beVisible, haveText, beChecked, haveAttribute, haveClasses, haveValue, notBeChecked, notHaveAttribute, notHaveClasses, test, html } from '../../utils'
+
+test('style attribute object binding',
+    html`
+        <div x-data>
+            <span x-bind:style="{ color: 'red' }">I should be red</span>
+        </div>
+    `,
+    ({ get }) => {
+        get('span').should(haveAttribute('style', 'color: red;'))
+    }
+)
+
+test('style attribute object binding using camelCase syntax',
+    html`
+        <div x-data>
+            <span x-bind:style="{ backgroundColor: 'red' }">I should be red</span>
+        </div>
+    `,
+    ({ get }) => {
+        get('span').should(haveAttribute('style', 'background-color: red;'))
+    }
+)
+
+test('style attribute object binding using kebab-case syntax',
+    html`
+        <div x-data>
+            <span x-bind:style="{ 'background-color': 'red' }">I should be red</span>
+        </div>
+    `,
+    ({ get }) => {
+        get('span').should(haveAttribute('style', 'background-color: red;'))
+    }
+)
+
+test('style attribute object binding with CSS variable',
+    html`
+        <div x-data x-bind:style="{ '--MyCSS-Variable': 0.25 }">
+            <span style="opacity: var(--MyCSS-Variable);">I should be hardly visible</span>
+        </div>
+    `,
+    ({ get }) => {
+        get('div').should(haveAttribute('style', '--MyCSS-Variable:0.25;'))
+    }
+)
+
+test('style attribute object bindings are merged with existing styles',
+    html`
+        <div x-data>
+            <span style="display: block" x-bind:style="{ color: 'red' }">I should be red</span>
+        </div>
+    `,
+    ({ get }) => {
+        get('span').should(haveAttribute('style', 'display: block; color: red;'))
+    }
+)
+
+test('CSS custom properties are set',
+    html`
+        <div x-data="{custom_color: '#f00'}">
+            <span style="color: var(--custom-prop)" x-bind:style="{ '--custom-prop': custom_color }">I should be red</span>
+        </div>
+    `,
+    ({ get }) => {
+        get('span').should(haveAttribute('style', 'color: var(--custom-prop); --custom-prop:#f00;'))
+    }
+)
+
+test('existing CSS custom properties are preserved',
+    html`
+        <div x-data="{link: 'var(--custom-prop-a)'}">
+            <span style="color: var(--custom-prop-b); --custom-prop-a: red" x-bind:style="{ '--custom-prop-b': link }">I should be red</span>
+        </div>
+    `,
+    ({ get }) => {
+        get('span').should(haveAttribute('style', 'color: var(--custom-prop-b); --custom-prop-a: red; --custom-prop-b:var(--custom-prop-a);'))
+    }
+)
+ */
