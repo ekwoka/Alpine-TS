d4eb888cfd40f2c8210c901a7f0183ab46765a7d
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-03-12 17:39:24 +0400

    :white_check_mark: Tests x-ignore

---
+++ tests/directives/x-ignore.test.ts
@@ -0,0 +1,30 @@
+import { render } from '../utils';
+
+describe('x-ignore', () => {
+  it('ignores the tree', async () => {
+    const { $ } = await render(
+      undefined,
+      `
+        <div x-data="{ foo: 'bar' }">
+            <div x-ignore>
+                <span x-text="foo"></span>
+            </div>
+        </div>
+      `
+    );
+    expect($('span').textContent).toBe('');
+  });
+  it('ignores self', async () => {
+    const { $ } = await render(
+      undefined,
+      `
+        <div x-data="{ foo: 'bar' }">
+            <div x-ignore.self x-text="foo">
+              <span x-text="foo"></span>
+            </div>
+        </div>
+      `
+    );
+    expect($('span').textContent).toBe('bar');
+  });
+});
