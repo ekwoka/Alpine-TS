d708c62f431292e209b3f62ce94aebf7d1d89cfd
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-04-24 10:38:25 +0300

    :white_check_mark: Tests $dispatch

---
+++ tests/magics/$dispatch.test.ts
@@ -0,0 +1,17 @@
+import { cleanTextContent, render } from '../utils';
+
+describe('$dispatch', () => {
+  it('dispatches an event', async () => {
+    const { $, click } = await render(
+      undefined,
+      `
+        <div x-data="{ foo: 'bar' }" x-on:dispatched-event="foo = $event.detail">
+          <button @click="$dispatch('dispatched-event', 'baz')" x-text="foo">click</button>
+        </div>
+      `
+    );
+    expect(cleanTextContent($('div').textContent)).toBe('bar');
+    await click('button');
+    expect(cleanTextContent($('div').textContent)).toBe('baz');
+  });
+});
