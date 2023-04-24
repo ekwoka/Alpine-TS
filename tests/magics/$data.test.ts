04058343ce8d30c50c0e6808ce2a443a3b731fa3
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-04-24 10:32:21 +0300

    :white_check_mark: Tests $data

---
+++ tests/magics/$data.test.ts
@@ -0,0 +1,18 @@
+import { cleanTextContent, render } from '../utils';
+
+describe('$data', () => {
+  it('returns the current merged scope', async () => {
+    const { $ } = await render(
+      undefined,
+      `
+        <div x-data="{ foo: 'bar' }">
+          <div x-data="{ fizz: 'buzz' }">
+            <span x-text="$data.foo"></span>
+            <span x-text="$data.fizz"></span>
+          </div>
+        </div>
+      `
+    );
+    expect(cleanTextContent($('div').textContent)).toBe('bar buzz');
+  });
+});
