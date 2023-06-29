31b8533807e02eb3d98bd3ef43689b761eb9b947
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-06-29 10:34:30 +0400

    :construction_worker: Uses Brotli instead of gzip

--- scripts/getSize.js
+++ scripts/getSize.js
@@ -1,10 +1,8 @@
 import { build } from 'esbuild';
-import { gzipSize } from 'gzip-size';
 import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
+import { brotliCompressSync } from 'node:zlib';
 import prettyBytes from 'pretty-bytes';
-
-const test = process.env.NODE_ENV === 'test';
 
 const packages = ['alpinejs'];
 
@@ -12,8 +10,7 @@ const bundleCode = async (pkg) => {
   const { outputFiles } = await build({
     entryPoints: [`./packages/${pkg}/src/index.js`],
     inject: [],
-    outfile: 'testing/bundle.ts',
-    write: test,
+    write: false,
     splitting: false,
     format: 'esm',
     bundle: true,
@@ -23,14 +20,12 @@ const bundleCode = async (pkg) => {
     plugins: [],
     mainFields: ['module', 'main'],
   });
-  if (test) return 'done';
-  const { minified, gzipped } = await getSizes(outputFiles[0].contents);
-  console.log(
-    `${pkg}: Bundle: ${minified.pretty}, Minzipped: ${gzipped.pretty}`
-  );
+  console.log(getSizes(outputFiles[0].contents));
+  const { minified, brotli } = getSizes(outputFiles[0].contents);
+  console.log(`${pkg}: Bundle: ${minified.pretty}, Brotli: ${brotli.pretty}`);
   return {
     minified,
-    gzipped,
+    brotli,
   };
 };
 
@@ -41,16 +36,15 @@ const sizeInfo = (bytesSize) => ({
 
 const getBytes = (str) => Buffer.byteLength(str, 'utf8');
 
-const getSizes = async (code) => {
+const getSizes = (code) => {
   const minifiedSize = getBytes(code);
-  const gzippedSize = await gzipSize(code);
+  const brotliSize = getBytes(brotliCompressSync(code));
 
-  return { minified: sizeInfo(minifiedSize), gzipped: sizeInfo(gzippedSize) };
+  return { minified: sizeInfo(minifiedSize), brotli: sizeInfo(brotliSize) };
 };
 
 const bundleData = await Promise.all(
   packages.map(async (pkg) => [pkg, await bundleCode(pkg)])
 );
-if (test) process.exit(0);
 const content = JSON.stringify(Object.fromEntries(bundleData), null, 2);
 await writeFile(join('size.json'), content, 'utf8');
