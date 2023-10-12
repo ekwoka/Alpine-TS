e829efa5f7956c05617fd6554c39329db7797fa9
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-10-12 08:50:36 +0400

    :bug: Fixes setter/getter pair bug

--- tests/scope.test.ts
+++ tests/scope.test.ts
@@ -20,4 +20,28 @@ describe('mergeProxies', () => {
     const proxy = mergeProxies(objects);
     expect(JSON.stringify(proxy)).toBe('{"foo":"bar","baz":"qux"}');
   });
+  it('allows getters and setters to access the merged proxy', () => {
+    const objects = [
+      { foo: 'bar' },
+      {
+        foo: 'buzz',
+        get fizz() {
+          return this.foo;
+        },
+        set fizz(value) {
+          this.foo = value;
+        },
+      },
+    ];
+    const proxy = mergeProxies(objects);
+    expect(proxy.foo).toBe('bar');
+    expect(proxy.fizz).toBe('bar');
+    proxy.fizz = 'baz';
+    console.log(JSON.stringify(proxy));
+    console.log(objects);
+    expect(proxy.foo).toBe('baz');
+    expect(proxy.fizz).toBe('baz');
+    expect(objects[0].foo).toBe('baz');
+    expect(objects[1].foo).toBe('buzz');
+  });
 });
