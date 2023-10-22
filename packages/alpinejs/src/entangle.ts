19ef7e55e8d001d44432850e1bdd280551200b62
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-10-22 15:11:07 +0400

    :bug: Fixes Livewire compatibility

--- packages/alpinejs/src/entangle.ts
+++ packages/alpinejs/src/entangle.ts
@@ -5,17 +5,27 @@ export const entangle = <T>(
   { get: innerGet, set: innerSet }: Entangler<T>,
 ) => {
   let firstRun = true;
-  let previous: T;
+  let previousHash: string | undefined;
 
   const reference = effect(() => {
     const outer = outerGet();
     const inner = innerGet();
     if (firstRun) {
-      innerSet(outer); // We need to break internal references using parse/stringify... (do we though?)
+      innerSet(cloneIfObject(outer));
       firstRun = false;
-      previous = outer;
-    } else if (outer !== previous) innerSet((previous = outer));
-    else outerSet((previous = inner));
+      previousHash = JSON.stringify(outer);
+    } else {
+      const latestHash = JSON.stringify(outer);
+      if (latestHash !== previousHash) {
+        innerSet(cloneIfObject(outer));
+        previousHash = latestHash;
+      } else {
+        outerSet(cloneIfObject(inner));
+        previousHash = JSON.stringify(inner);
+      }
+    }
+    JSON.stringify(innerGet());
+    JSON.stringify(outerGet());
   });
 
   return () => {
@@ -26,4 +36,8 @@ export const entangle = <T>(
 type Entangler<T> = {
   get: () => T;
   set: (value: T) => void;
+};
+
+const cloneIfObject = (value: unknown) => {
+  return typeof value === 'object' ? JSON.parse(JSON.stringify(value)) : value;
 };
