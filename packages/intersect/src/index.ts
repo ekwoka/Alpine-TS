8f3b6e26ccaeeafcc0804b9ed003f1e9e336d939
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-10-29 17:46:14 +0400

    :label: Rewrites Intersect

---
+++ packages/intersect/src/index.ts
@@ -0,0 +1,74 @@
+import type { PluginCallback } from 'alpinets';
+
+export const intersectFunction: PluginCallback = (Alpine) => {
+  Alpine.directive(
+    'intersect',
+    (el, { value, expression, modifiers }, { evaluateLater, cleanup }) => {
+      const evaluate = evaluateLater(expression);
+
+      const options: IntersectionObserverInit = {
+        rootMargin: getRootMargin(modifiers),
+        threshold: getThreshold(modifiers),
+      };
+
+      const once = modifiers.includes('once');
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          // Ignore entry if intersecting in leave mode, or not intersecting in enter mode
+          if (entry.isIntersecting === (value === 'leave')) return;
+
+          evaluate();
+
+          once && observer.disconnect();
+        });
+      }, options);
+
+      observer.observe(el);
+
+      cleanup(() => {
+        observer.disconnect();
+      });
+    },
+  );
+};
+
+export const getThreshold = (modifiers: string[]) => {
+  if (modifiers.includes('full')) return 0.99;
+  if (modifiers.includes('half')) return 0.5;
+  if (!modifiers.includes('threshold')) return 0;
+
+  const threshold = modifiers[modifiers.indexOf('threshold') + 1];
+
+  if (threshold === '100') return 1;
+  if (threshold === '0') return 0;
+
+  return Number(`.${threshold}`);
+};
+
+export const getLengthValue = (rawValue: string) => {
+  // Supported: -10px, -20 (implied px), 30 (implied px), 40px, 50%
+  const match = rawValue.match(/^(-?[0-9]+)(px|%)?$/);
+  return match ? match[1] + (match[2] || 'px') : null;
+};
+
+const fallback = '0px 0px 0px 0px';
+export const getRootMargin = (modifiers: string[]) => {
+  const key = 'margin';
+  const index = modifiers.indexOf(key);
+
+  // If the modifier isn't present, use the default.
+  if (index === -1) return fallback;
+
+  // Grab and parse the 4 subsequent length values after margin modifier: x-intersect.margin.300px.0.50%.0
+  return (
+    modifiers
+      .slice(index + 1, index + 5)
+      .map(getLengthValue)
+      .filter(outUndefined)
+      .join(' ')
+      .trim() || fallback
+  );
+};
+
+const outUndefined = <T>(value: T | undefined): value is T => value !== null;
---
+++ packages/intersect/vite.config.ts
@@ -0,0 +1,51 @@
+/// <reference types="vitest" />
+import { accessOwnSources } from '../../plugins/accessOwnPlugins';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  root: resolve(__dirname),
+  plugins: [
+    dts({
+      entryRoot: resolve(__dirname, 'src'),
+      tsconfigPath: resolve(__dirname, 'tsconfig.json'),
+    }),
+    tsconfigPaths(),
+    accessOwnSources(),
+  ],
+  define: {
+    'import.meta.vitest': 'undefined',
+    'import.meta.DEBUG': 'false',
+  },
+  build: {
+    target: 'esnext',
+    outDir: resolve(__dirname, 'dist'),
+    lib: {
+      entry: resolve(__dirname, 'src', 'index.ts'),
+      formats: ['es'],
+    },
+    minify: false,
+    rollupOptions: {
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: ({ name: fileName }) => {
+          return `${fileName}.js`;
+        },
+        sourcemap: true,
+      },
+      external: [/node_modules/],
+    },
+  },
+  test: {
+    globals: true,
+    include: ['./**/*{.spec,.test}.{ts,tsx}'],
+    includeSource: ['./**/*.{ts,tsx}'],
+    reporters: ['dot'],
+    deps: {},
+    useAtomics: true,
+    passWithNoTests: true,
+  },
+});
